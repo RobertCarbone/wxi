cd44fc05cb6ec1a91409da6347e4ccea7fd10832
Dmitry Golubovsky <golubovsky@gmail.com>
2010-03-24 05:27:55 -0400

    Added example 3 with two buttons in parallel and message mapper.

---
+++ examples/ex3.erl
@@ -0,0 +1,17 @@
+%% The wxi library example 2: two buttons and message mapper.
+
+-module(ex3).
+
+-include_lib("wxi.hrl").
+
+-define(ID_PLUS, 100).
+-define(ID_MINUS, 200).
+
+-export([start/0]).
+
+start() ->
+    wxi:topFrame("WXI Example 3", 300, 100, ?wxHORIZONTAL, 
+        {[wxi:button("+", ?ID_PLUS), wxi:button("-", ?ID_MINUS)], 
+         wxi:map(fun (#wx {id = I}) -> I end),
+         wxi:textLabel("~p", "0")}).
+
